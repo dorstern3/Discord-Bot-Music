5bd322872ca11587d5f7b08197a8ae2f470a9305
dor <dorstern3@gmail.com>
2022-08-18 20:40:55 +0300

    try

--- main.py
+++ main.py
@@ -17,48 +17,47 @@ client = commands.Bot(command_prefix="/")
 
 # Connect voice Channel
 @client.command()
-async def connect(ctx,url_: str):
+async def connect(ctx):
   voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
   voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-  # if not voice.is_connected():
-  await voiceChannel.connect()
-  print("Connect voice channel")
+  if voice is None or not voice.is_connected() :
+    await voiceChannel.connect()
+    await ctx.send("Connect voice channel")
+  else:
+    await ctx.send("The Music-Bot is not connected a voice channel")
 
 # Disconnect voice Channel
 @client.command()
 async def disconnect(ctx):
   voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-  # if voice.is_connected():
   await voice.disconnect()
-  print("Disconnect voice channel")
-  # else:
-  #   await ctx.send("the bot is not connect")
+  await ctx.send("Disconnect voice channel")
 
 #Stop
-async def stop(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-  voice.stop()
+# async def stop(ctx):
+#   voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#   voice.stop()
  
 #Pause
-@client.command()
-async def pause(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-  if voice.is_playing():
-    voice.pause()
-    print("voice pause")
-  else:
-    print("voice no pause")
+# @client.command()
+# async def pause(ctx):
+#   voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#   if voice.is_playing():
+#     voice.pause()
+#     print("voice pause")
+#   else:
+#     print("voice no pause")
     # await ctx.send("voice no pause")
 
 #Resume
-@client.command()
-async def resume(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-  if voice.is_paused():
-    voice.resume()
-    print("voice resume")
-  else:
-    print("voice no resume")
+# @client.command()
+# async def resume(ctx):
+#   voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#   if voice.is_paused():
+#     voice.resume()
+#     print("voice resume")
+#   else:
+#     print("voice no resume")
     # await ctx.send("voice no resume")
 
 
