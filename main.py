fb3e9314e012845d1f51033ba23620eed044a01d
dor <dorstern3@gmail.com>
2022-09-01 13:54:47 +0300

    90%

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
 import os
-
+# youtube
+import youtube_dl
 # Commands
 from discord.ext import commands
-
 # TOKEN
 from dotenv import load_dotenv
 
@@ -12,22 +12,51 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 print(TOKEN)
 
-client = commands.Bot(command_prefix="/")
+bot = commands.Bot(command_prefix="/")
 
 # Connect voice Channel
-@client.command()
+@bot.command()
 async def connect(ctx):
-  voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-  if voice is None or not voice.is_connected():
-    await voiceChannel.connect()
-    await ctx.send("Connect voice channel")
+ voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
+ voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+ 
+ if voice is None or not voice.is_connected():
+  await voiceChannel.connect()
+  await ctx.send("Connect voice channel")
 
+# Play music
+@bot.command()
+async def play(ctx, url_: str):
+  song = os.path.isfile("song.mp3")
+  try:
+      if song:
+          os.remove("song.mp3")
+  except PermissionError:
+      await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+      return
+
+  # voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+  ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+  for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+  voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 # Disconnect voice Channel
-@client.command()
+@bot.command()
 async def disconnect(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
   if voice.is_connected():
    await voice.disconnect()
    await ctx.send("Disconnect voice channel")
@@ -36,14 +65,14 @@ async def disconnect(ctx):
 
 #Stop
 async def stop(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
   voice.stop()
   await ctx.send("Audio stop")
  
 # Pause
-@client.command()
+@bot.command()
 async def pause(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
   if voice.is_playing():
     voice.pause()
     await ctx.send("Audio pause")
@@ -51,64 +80,13 @@ async def pause(ctx):
     await ctx.send("Audio not playing")
 
 #Resume
-@client.command()
+@bot.command()
 async def resume(ctx):
-  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
   if voice.is_paused():
     voice.resume()
     await ctx.send("Audio resume")
   else:
     await ctx.send("The audio is not paused")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @client.command()
-# async def hello(self,ctx):
-#   await ctx.send('hello1 ')
-
-# client=discord.Client()
-#Command
-# @client.event
-# async def on_ready():
-#     print('We have logged in as {0.user}'
-#     .format(client))
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('/command'):
-#       await message.channel.send('command')
-
- 
-
-  
-client.run(TOKEN)
+bot.run(TOKEN)
